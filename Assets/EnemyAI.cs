0c80d150cc6bf47d957c7560f84e72bc3a616597
Brendan Ritter <brendan.ritter@yahoo.com>
2014-11-08 18:13:24 -0500

    Added some enemy AI. Need to move target around to make dynamic.

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -1,26 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyAI : MonoBehaviour {
+public class EnemyAI : MonoBehaviour
+{
 
 	private bool isPlayingSound = false;
+	public GameObject m_target;
+	public float m_speed = 5;
 
-	IEnumerator playSound(){
+	IEnumerator playSound ()
+	{
 		isPlayingSound = true;
-		yield return new WaitForSeconds(3f);
+		yield return new WaitForSeconds (3f);
 		isPlayingSound = false;
 		audio.Play ();
 		Debug.Log ("Play Sound");
 	}
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		if (!isPlayingSound) {
 			StartCoroutine (playSound ());
+		}
+		moveToTarget ();
+	}
+	void moveToTarget ()
+	{
+		Vector2 dif = m_target.transform.position - transform.position;
+		if ((dif).magnitude > 1) {
+			rigidbody2D.AddForce (dif * m_speed);
 		}
 	}
 }
