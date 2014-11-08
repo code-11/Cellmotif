a3415c9da55beb7232b9d918b9f62a0372843a6e
Chloe Eghtebas <nceghtebas@hotmail.com>
2014-11-08 16:32:29 -0500

    Added wasd movement

---
+++ Assets/absorbController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class absorbController : MonoBehaviour {
+
+//	public float m_radius =
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/movementController.cs
+++ Assets/movementController.cs
@@ -2,17 +2,38 @@
 using System.Collections;
 
 public class movementController : MonoBehaviour
-{
-
+{	
+		public float m_speed =10;
+		public float m_dragspeed =5;
+	
+		
 		// Use this for initialization
 		void Start ()
 		{
-	
+			
 		}
 	
 		// Update is called once per frame
 		void Update ()
 		{
-				bool upKey = Input.GetKey ("w");
+			bool upKey = Input.GetKey ("w");
+			bool downKey= Input.GetKey ("s");
+			bool leftKey = Input.GetKey ("a");
+			bool rightKey = Input.GetKey ("d");
+
+			rigidbody2D.AddForce(-rigidbody2D.velocity.normalized * m_dragspeed);
+			if (upKey){
+				rigidbody2D.AddForce(transform.up.normalized * m_speed);
+			}
+			if (downKey) {
+				rigidbody2D.AddForce(-transform.up.normalized * m_speed);
+			}
+			if (leftKey) {
+				rigidbody2D.AddForce(-transform.right.normalized * m_speed);
+			}
+			if (rightKey) {
+				rigidbody2D.AddForce(transform.right.normalized * m_speed);
+			}
+				
 		}
 }
