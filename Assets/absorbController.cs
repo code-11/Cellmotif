77c82199a1b54ce29eab6861e7d9f61749d33d97
Chloe Eghtebas <nceghtebas@hotmail.com>
2014-11-08 17:01:00 -0500

    Added enemy, enemy collision will kill enemy and increase your size

--- Assets/absorbController.cs
+++ Assets/absorbController.cs
@@ -3,15 +3,21 @@ using System.Collections;
 
 public class absorbController : MonoBehaviour {
 
-	public float m_radius = 5 ;
+	public float m_growFactor = 1.5f ;
 
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
+	void OnCollisionEnter2D(Collision2D hit){
+		Debug.Log ("Hit");
+		Destroy (hit.gameObject);
+		gameObject.transform.localScale = gameObject.transform.localScale * m_growFactor;
+	}
+
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 }
