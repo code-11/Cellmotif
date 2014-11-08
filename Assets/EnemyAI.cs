5f0e68f16f60f43a6890f59cae4356afa0b9bca6
Chloe Eghtebas <nceghtebas@hotmail.com>
2014-11-08 17:32:49 -0500

    enemy plays sound repeatedly

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+
+	private bool isPlayingSound = false;
+
+	IEnumerator playSound(){
+		isPlayingSound = true;
+		yield return new WaitForSeconds(3f);
+		isPlayingSound = false;
+		audio.Play ();
+		Debug.Log ("Play Sound");
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isPlayingSound) {
+			StartCoroutine (playSound ());
+		}
+	}
+}
