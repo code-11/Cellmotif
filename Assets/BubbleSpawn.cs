d19a2132a16b7f5e244a6b567ed0404d34d665e2
Emily Tumang <emily.tumang@students.olin.edu>
2014-11-09 01:03:20 -0500

    Various features.

--- Assets/BubbleSpawn.cs
+++ Assets/BubbleSpawn.cs
@@ -9,7 +9,7 @@ public class BubbleSpawn : MonoBehaviour {
 		for (int i = -7; i<7; i+=1) {
 			for(int j = -7; j<7; j+=1){
 						Vector3 position = new Vector3(3*(i+Random.value), 3*(j+Random.value), 4);
-						Quaternion rotation = Quaternion.identity;
+						Quaternion rotation = new Quaternion(Random.value, Random.value, 0, 0);
 						
 
 						Instantiate (bubble, position, rotation);
--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -11,24 +11,52 @@ public class EnemyAI : MonoBehaviour
 	public Sprite normal;
 	private SpriteRenderer Srenderer;
 
+	public float m_growFactor = 1.5f;
+
 	IEnumerator playSound ()
 	{	
 		isPlayingSound = true;
+		StartCoroutine (pulse());
 		yield return new WaitForSeconds (3f);
-		isPlayingSound = false;
 		audio.Play ();
+		isPlayingSound = false;
 		Debug.Log ("Play Sound");
 	}
 
-	/*IEnumerator pulse(){
+	IEnumerator pulse()
+	{
 		if (Srenderer.sprite == normal) {
 						Srenderer.sprite = highlighted;
 				}
 		else {
 						Srenderer.sprite = normal;
 				}
+		yield return new WaitForSeconds (.5f);
+		Debug.Log ("Change sprite");
+		if (isPlayingSound) {
+						StartCoroutine (pulse());
+				}
 
-		}*/
+	}
+
+	void OnCollisionEnter2D(Collision2D hit){
+		EnemyAI prey = hit.gameObject.GetComponent<EnemyAI> ();
+		ObjectSize enemy_size = gameObject.GetComponent<ObjectSize> ();
+		
+		if (prey.GetComponent<ObjectSize>().size < enemy_size.size) {
+			Debug.Log ("Hit");
+			Destroy (prey.m_target);
+			Destroy (hit.gameObject);
+			
+			gameObject.transform.localScale = gameObject.transform.localScale * m_growFactor;
+			enemy_size.setSize (enemy_size.size * m_growFactor);
+		} 
+
+		else {
+			Destroy (gameObject);
+		}
+		
+	}
 
 	// Use this for initialization
 	void Start ()
@@ -41,7 +69,9 @@ public class EnemyAI : MonoBehaviour
 	{
 		if (!isPlayingSound) {
 			StartCoroutine (playSound());
-			//StartCoroutine (pulse());
+		}
+		if (Srenderer.sprite == highlighted) {
+			Srenderer.sprite = normal;
 		}
 		moveToTarget ();
 	}
